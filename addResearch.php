6c5ed17e2dcea4f73cf74dc2926d837123ef2ea5
loydanthony <freddiemunio@gmail.com>
2018-07-19 02:19:38 +0800

    Loyde Ui

--- addResearch.php
+++ addResearch.php
@@ -1,9 +1,13 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" type="text/css" href="addstyle.css">
     <title>Add Research Document - BUKSU RRMS</title>
 </head>
 <body>
@@ -15,8 +19,35 @@
             <li><a href = "Login.php">Login</a></li>
         </ul>
     </div>
+    <div class="header">
+      <h1>Add Document</h1>
+    </div>
     <div id="content">
-        Place The content Here.hghghhghghghfghfghhhx
+       <div id ="forms">
+         <form id="form-group" action ="add.php"method="post">
+           <center><input type="text" name="book_title" placeholder="Book title" ></center><br><br>
+           <center><textarea rows="4" cols="20" placeholder="abstract"></textarea></center><br><br>
+           <input type="date" name="pubdate" placeholder=""><br><br>
+
+           <select name="category">
+             <?php include 'dbconfig.php';
+             $query = "SELECT * FROM `category`";
+             $result = $conn->query($query);
+
+             if ($result->num_rows > 0) {
+               while($row=$result->fetch_assoc()){?>
+                 <option><?php echo $row["cat_name"]; ?></option>
+               <?php
+             }
+             }
+              ?>
+           </select>
+          <input type="submit" name="btn-submit">
+        </forms>
+
+
+
+       </div>
     </div>
 </body>
 </html>
