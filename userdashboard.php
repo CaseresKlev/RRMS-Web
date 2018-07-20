2cd24c80ca5157f9e5dcb2b2ba2a65a73deacad3
himeTapayan <himesama.labuntog22@gmail.com>
2018-07-20 12:03:30 +0800

    Princess Dashboards

--- userdashboard.php
+++ userdashboard.php
@@ -1,14 +1,88 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title> User </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-   // <script src="main.js"></script>
+    <!-- <script src="main.js"></script>
+	<!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/font.min.css">
+    <!-- bootstrap-progressbar -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/progressbar.min.css">
+    <!-- Custom Theme Style -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/custom.min.css">
+	
 </head>
-<body>
-    THis is Body.
-</body>
+<body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="userdashboard.php" class="site_title"> <span> RRMS </span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="final.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span> USERNAME </span>
+                <h2> Student </h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-group"></i> MY BOOKS <span class="fa fa-chevron-down"></span></a>
+                  </li>
+                  <li><a><i class="fa fa-user"></i> MY BIO </a>
+                  <li><a><i class="fa fa-envelope"></i> REVISIONS </a>         
+                  <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> LOGOUT <span class="label label-success pull-right"></a></li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </div>
+       
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <!-- top tiles -->
+          <div class="row tile_count"></div>
+          <!-- /top tiles -->
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+  
+    <!-- bootstrap-progressbar -->
+    <script src="js/bootstrap-progressbar.min.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="js/custom.min.js"></script>
+	
+  </body>
 </html>
