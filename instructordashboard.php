d98035087f22c7b474960bd339b6d02077497e74
CaseresKlev <klevmailbox04@gmail.com>
2018-07-24 20:56:20 +0800

    Revert "Merge branch 'master' of https://github.com/CaseresKlev/RRMS-Web-Repo"
    This reverts commit aa69e42d971be5b9cc3de080a8223e7eacb9e4e1, reversing
    changes made to e54779186cd6979402dc9ee3f2ad4b224ad9814f.

--- instructordashboard.php
+++ instructordashboard.php
@@ -6,8 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title> Administrator </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <!-- <script src="main.js"></script>
+	<!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/font.min.css">
+    <!-- bootstrap-progressbar -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/progressbar.min.css">
     <!-- Custom Theme Style -->
-    <link rel="stylesheet" type="text/css" media="screen" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/custom.min.css">
 	
 </head>
 
@@ -16,8 +24,8 @@
       <div class="main_container">
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
-            <div class="navbar nav_title">
-              <a href="instructordashboard.php" class="site_title"><span class= "glyphicon glyphicon-home"> RRMS </span></a>
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="instructordashboard.php" class="site_title"> <span> RRMS </span></a>
             </div>
 
             <div class="clearfix"></div>
@@ -25,7 +33,7 @@
             <!-- menu profile quick info -->
             <div class="profile clearfix">
               <div class="profile_pic">
-                <img class="img-circle" src="img/final.jpg" alt="...">
+                <img src="final.jpg" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
                 <span> USERNAME </span>
@@ -33,20 +41,23 @@
               </div>
             </div>
             <!-- /menu profile quick info -->
-				<br/>
+
+            <br />
+
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <div class="nav side-menu">
-                  <a href= "#myclass"><span class="glyphicon glyphicon-user"></span> MY CLASS <span class="fa fa-chevron-down"></span></a></br>
-                    <div class="nav child_menu">
-						<a href= "#add"> Add </a></br>
-						<a href= "#del"> Delete </a> 
-                    </div> </br>
-					<a href="#adviser"><span class="glyphicon glyphicon-user"></span> ADVISER </a> </br></br>
-					<a href="#account"><span class="glyphicon glyphicon-envelope"></span> ACCOUNTS </a>  </br></br>  
-					<a href="javascript:void(0)"><span class="glyphicon glyphicon-off"></span> LOGOUT </a>
-                </div>
+                <ul class="nav side-menu">
+                  <li><a><span class="glyphicon glyphicon-user"></span> MY CLASS <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a> Add </a></li>
+                      <li><a> Delete </a></li>
+                    </ul>
+                  </li>
+                  <li><a><span class="glyphicon glyphicon-user"></span> ADVISER </a>
+                  <li><a><span class="glyphicon glyphicon-envelope"></span> ACCOUNTS </a>         
+                  <li><a href="javascript:void(0)"><span class="glyphicon glyphicon-off"></span> LOGOUT <span class="label label-success pull-right"></a></li>
+                </ul>
               </div>
 
             </div>
@@ -54,14 +65,18 @@
         </div>
        
         <!-- page content -->
-        <div class="right_col" style= "height: 100%">
-          <!-- top tiles --
+        <div class="right_col" role="main">
+          <!-- top tiles -->
           <div class="row tile_count"></div>
           <!-- /top tiles -->
               </div>
             </div>
           </div>
+        </div>
         <!-- /page content -->
+
+      </div>
+    </div>
 
     <!-- jQuery -->
     <script src="js/jquery.min.js"></script>
