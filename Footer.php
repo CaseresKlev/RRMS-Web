cc351111b864c0c10441107473a3ba18a0028a8c
himeTapayan <himesama.labuntog22@gmail.com>
2018-07-25 14:03:02 +0800

    Princess Header and Footer

---
+++ Footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<title> Footer </title>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href= "header&footercss.css">
+</head>
+<body">
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+</body>
+</html> 
