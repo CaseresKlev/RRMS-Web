b6d2ea71d4ab1520901616012db3567c3aeeeecf
loydanthony <freddiemunio@gmail.com>
2018-07-28 21:07:00 +0800

    book details

---
+++ imagedetails.php
@@ -0,0 +1,44 @@
+<docty html>
+
+  <html>
+
+  <head>
+    <meta charset="utf-8">
+    <title>IMAGE DETAILS</title>
+    <link rel="stylesheet"type="text/css"href="imagedetails.css">
+  </head>
+<body>
+  <?php
+    include("header.php");
+    ?>
+
+  <div class="box">
+    <div style="height:229px;"class="imgbox">
+      <img src="img/1.jpg"  >
+    </div>
+    <div class="content">
+      <h2>The book of love</h2>
+      <p><strong>Authors:</strong> Loyd anthony et al.</p>
+        <p><strong>pubdate:</strong> 1/2/2</p>
+
+      <p><strong>keywords:</strong> bla bla wffj ffjefawbgdfgdfsg dgsdsg gsdgsd gdsg sg sd fjpf sfjsd fjsd osdfsh fd d sd dfdbsf ddd bfsdjs dkf akd a fd sakdhbkfbsafbd skfa dbdbfdhsbfd bfdhbdhfbdf dbfdfbdfh dbd</p  >
+
+        <p><strong>Views:</strong>20</p>
+      </div>
+
+      <div class="lower">
+        <p><strong>revision:</strong>none</p>
+        <div class="abs">
+        <p><strong>abstract:</strong>  s  cd ssdcdsdg sdff weffe fef fefewf efwf wfwef fw fwefwefe ewfew fewfw dsf fsdf  mvdsklv gfjfd gj ds gjsdlksdjsd sdjfsd fjsdk sdfsdkfsdhfsd k vsmkv vmdk skvmsvds ds djssdbsdbf bbjfbdsfbdsfbsfb sdjfb dsfb sdfbdjfbds sdbdjhffj sdfksdbfsdjfbsdjfbsd kf</p>
+      </div>
+      <p><strong>References:</strong><a href="https://www.w3schools.com/html/">https://www.w3schools.com/html/</a></p>
+
+    </div>
+    </div>
+
+
+
+
+
+</body>
+</html>
