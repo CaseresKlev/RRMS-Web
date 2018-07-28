28b00abb825f10295eea61960e80f9f823faec3c
himeTapayan <himesama.labuntog22@gmail.com>
2018-07-28 21:42:50 +0800

    Header 7/28/18

--- Header.php
+++ Header.php
@@ -8,13 +8,16 @@
 
 <body style= "width: 70%">
 <div class="header" style= "width= 100%">
+
   <strong><center><a class="banner">Research Record Management System</a></center></strong>
+  
   <div class="nav">
 	<a href="#home">Home</a>
     <a href="#contact">Contact</a>
     <a href="#about">About</a>
 	<a href="#login">Login</a>
   </div>
+  
 </div>
 </body>
 </html>
