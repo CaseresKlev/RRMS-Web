13d9a1fcff78bbcffef88e2240a878e4b11eea9e
Rayanne Cruz <rayannepelincruz@gmail.com>
2018-08-05 18:43:17 +0800

    author UI

--- author.php
+++ author.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <!-- ANNE -->
 <html>
@@ -8,53 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Author - BUKSU RRMS</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/author.css">
+	<link rel="stylesheet" type="text/css" href="css/author-new.css">
 </head>
-<body>
-
-  <div class="author">
-      <fieldset>
-        <legend> AUTHOR </legend>
-          <form method="post" action="author-process.php">
-
-               <div class="input-group">
-                  <label>First name</label>
-                    <input type="text" name="firstname" value="" size="30" placeholder="Firstname">
-              </div>
-              <div class="input-group">
-                <label>Middle name</label>
-                  <input type="text" name="midname" value="" size="30" placeholder="Middlename">
-              </div>
-              <div class="input-group">
-                <label>Last name</label>
-                  <input type="text" name="lastname" value="" size="30" placeholder="Lastname">
-              </div>
-              <div class="input-group">
-                <label>Suffix</label>
-                  <input type="text" name="suffix" value="" size="10" placeholder="Suffix">
-              </div>
-              <div class="input-group">
-                <label>Address</label>
-                  <input type="text" name="add" value="" size="30" placeholder="Address">
-              </div>
-              <div class="input-group">
-                <label>Email</label>
-                  <input type="text" name="email" value="" size="20" placeholder="Email">
-              </div>
-
-              <div class="container">
-                  <img src="/RRMS-WEB/img/images.png" alt="author" class="image" style="width:128px; height:128px;"/>
-              </div>
-
-      </fieldset>
+<body id="annebody">
 
 
-      <input type="submit" value="Submit">
-
+  <div class="anneauthor">
+      <h1> KLEVIE JUN R. CASERES  </h1>
+      <p> 09168246985
+      <br> klevly@gmail.com
+      <br> Malaybalay City </p>
   </div>
 
+          <div class="vertiline"> </div>
 
+                <div class="annebooks">
+                  <h2> AUTHORED RESEARCH PAPER </h2>
+                </div>
 
+                  <div class="annelist">
+                      <ul>
+                
+                        <li> RESEARCH PAPER #1 </li> <br>
+                        <li> RESEARCH PAPER #2 </li> <br>
+                        <li> RESEARCH PAPER #3 </li>
+
+                  </div>
 
 
 </body>
