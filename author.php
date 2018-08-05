5be21e270f491c929dafe613c4da8d52f2f9447d
Rayanne Cruz <rayannepelincruz@gmail.com>
2018-08-06 01:59:18 +0800

    final author.php-modification on UI

--- author.php
+++ author.php
@@ -12,15 +12,32 @@
 <body id="annebody">
 
   <?php
-  include('header.php');
+
+
+  $auth_id = $_GET['aut_id'];
+
 ?>
 
 
   <div class="anneauthor">
-      <h2> KLEVIE JUN R. CASERES  </h2>
-      <p> 09168246985
-      <br> klevly@gmail.com
-      <br> Malaybalay City </p>
+    <?php
+          include_once 'connection.php';
+          $dbconfig = new dbconfig();
+          $con = $dbconfig->getCon();
+          $query = "SELECT * FROM `author` WHERE a_id = $auth_id";
+          $result = $con->query($query);
+
+          if ($result->num_rows>0) {
+            while ($row=$result->fetch_assoc()) {
+              #echo $row['a_id'] . " " . $row['a_fname'] . " " . $row['a_mname'] . " " . $row['a_lname'] . " " . $row['a_suffix'] . " " . $row['a_add'] . " " . $row['a_contact'] . " " . $row['a_email'];
+
+     ?>
+      <h2><?php echo $row['a_fname'] . " " . $row['a_mname'] . " " . $row['a_lname'] . " " . $row['a_suffix']; ?></h2>
+      <p> <?php echo $row['a_contact']; ?>
+      <br> <?php echo $row['a_email']; ?>
+      <br> <?php echo $row['a_add']; ?> </p>
+    <?php }
+  } ?>
   </div>
           <div class="annebooks">
               <h3> AUTHORED RESEARCH PAPER </h3>
@@ -29,9 +46,24 @@
 
             <div class="annelist">
                       <ul>
-                        <li> RESEARCH PAPER #1 </li> <br>
-                        <li> RESEARCH PAPER #2 </li> <br>
-                        <li> RESEARCH PAPER #3 </li>
+                        <?php
+                          include_once 'connection.php';
+                          $dbconfig = new dbconfig();
+                          $con = $dbconfig->getCon();
+                          $query = "SELECT book.book_id, book.book_title FROM book INNER JOIN junc_authorbook ON book.book_id = junc_authorbook.book_id WHERE junc_authorbook.aut_id = $auth_id";
+                          $result = $con->query($query);
+
+                              if ($result->num_rows>0) {
+                                while ($row=$result->fetch_assoc()) {
+
+                           ?>
+
+
+                        <a href="bookdetails.php?book_id=<?php echo $row['book_id']; ?>"><li> <?php echo $row['book_title']; ?> </li> </a> <br>
+
+                      <?php }
+                    } ?>
+
                       </ul>
                   </div>
 
