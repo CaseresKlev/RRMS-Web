eb0c5dd75f58708d71ec13dfe9542a97c4bbd34e
Rayanne Cruz <rayannepelincruz@gmail.com>
2018-08-06 00:47:17 +0800

    author UI modified

--- author.php
+++ author.php
@@ -4,33 +4,35 @@
 <head>
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
     <title>Author - BUKSU RRMS</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/author-new.css">
 </head>
 <body id="annebody">
 
+  <?php
+  include('header.php');
+?>
+
 
   <div class="anneauthor">
-      <h1> KLEVIE JUN R. CASERES  </h1>
+      <h2> KLEVIE JUN R. CASERES  </h2>
       <p> 09168246985
       <br> klevly@gmail.com
       <br> Malaybalay City </p>
   </div>
+          <div class="annebooks">
+              <h3> AUTHORED RESEARCH PAPER </h3>
+                  <hr>
+          </div>
 
-          <div class="vertiline"> </div>
-
-                <div class="annebooks">
-                  <h2> AUTHORED RESEARCH PAPER </h2>
-                </div>
-
-                  <div class="annelist">
+            <div class="annelist">
                       <ul>
-                
                         <li> RESEARCH PAPER #1 </li> <br>
                         <li> RESEARCH PAPER #2 </li> <br>
                         <li> RESEARCH PAPER #3 </li>
-
+                      </ul>
                   </div>
 
 
