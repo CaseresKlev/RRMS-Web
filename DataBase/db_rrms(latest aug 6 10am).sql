19fd6a37cd00773aba1e4fa67ca8ebe321e8156d
loydanthony <freddiemunio@gmail.com>
2018-08-06 22:13:56 +0800

    latest db aug 6 10pm

---
+++ DataBase/db_rrms(latest aug 6 10am).sql
@@ -0,0 +1,642 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 06, 2018 at 04:08 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_rrms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `g_name` varchar(25) NOT NULL,
+  `u_name` varchar(12) NOT NULL,
+  `password` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`id`, `g_name`, `u_name`, `password`) VALUES
+(9, 'group1', 'loyd', '1234'),
+(11, 'rg', 'ggg', 'ggfdgfg'),
+(12, 'dffsd', 'fdsf', 'sdfdfdf'),
+(13, 'fefe', 'fsdfdsf', 'sdfdff'),
+(18, 'vdvv', 'vdsvdv', 'vdsv'),
+(19, 'efwfwefewffwf', 'wefewfewfw', 'wefewf'),
+(20, 'voidmain', 'loyds', '1234'),
+(21, 'aysxasyuxg', 'cgcg', 'asgas'),
+(22, 'dsfdsf', 'fffff', 'fddd'),
+(24, 'dsd', 'sadasd', 'asdasd'),
+(25, 'add', 'sdad', 'asd'),
+(26, 'dfsdsf', 'fdsf', 'cxfcxcvcv'),
+(27, 'trhytr', 'ewqewqwe', 'qwwqwq'),
+(32, 'dfsf', 'loydsds', '1234'),
+(33, 'eff', 'fdsfdfs', 'dfddd'),
+(35, 'eff', 'fdsfdfsrgre', 'dfddd'),
+(36, 'fgfdfd', 'fdfdfdsf', 'sdfsdfsdf'),
+(37, 'yfuyfyu', 'fuyfyu', 'fuyfufu'),
+(38, 'fft', 'ghfhgf', 'fhgfhgf'),
+(39, 'gsyugs', 'guyg', 'gyugyu'),
+(40, 'hhu', 'uihuih', 'huihuihiu'),
+(41, 'trrry', 'tryryt', 'rtyrtyr'),
+(43, 'dfdf', 'dfdsf', 'sdfdsf'),
+(44, 'fdfsdf', 'sdfsdff', 'sdfsdf'),
+(45, 'rrtt', 'thtrh', 'rthrth'),
+(46, 'dsvv', 'vsdvv', 'svv'),
+(47, 'group', 'ggg', 'regrgreg'),
+(48, 'rgegeg', 'regergeg', 'ergerge'),
+(49, 'ffffs', 'sdfsdfdsfsf', 'sdfsdfdsf'),
+(50, 'fffddf', 'fdhfdhdh', 'fdhfdhd'),
+(52, 'fffddf', 'fdhfdhdhdf', 'fdhfdhd'),
+(53, 'fdsfsdf', 'dsfsdfs', 'sdfdfsdf'),
+(54, 'fdsfdsfdf', 'dsfD', 'dfdsfdsf'),
+(56, 'dfsfsdf', 'dfsfsdfd', 'sdfsdfsfds'),
+(58, 'fsfdfd', 'sdfdsf', 'sdffsdfsf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adviser`
+--
+
+CREATE TABLE `adviser` (
+  `adv_id` smallint(6) NOT NULL,
+  `adv_fname` varchar(20) NOT NULL,
+  `adv_midName` varchar(20) NOT NULL,
+  `adv_Lname` varchar(20) NOT NULL,
+  `adv_suff` varchar(5) NOT NULL,
+  `adv_email` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `adviser`
+--
+
+INSERT INTO `adviser` (`adv_id`, `adv_fname`, `adv_midName`, `adv_Lname`, `adv_suff`, `adv_email`) VALUES
+(1, 'Sales', 'Gamponia', 'Aribe', 'JR', 'drftfhgyjuh@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authentication`
+--
+
+CREATE TABLE `authentication` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `type` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `authentication`
+--
+
+INSERT INTO `authentication` (`id`, `username`, `password`, `type`) VALUES
+(1, 'anne', 'anne', 'admin'),
+(2, 'ambot', 'ambot', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `a_id` int(11) NOT NULL,
+  `a_fname` varchar(20) NOT NULL,
+  `a_mname` varchar(20) NOT NULL,
+  `a_lname` varchar(20) NOT NULL,
+  `a_suffix` varchar(5) NOT NULL,
+  `a_add` varchar(50) NOT NULL,
+  `a_contact` varchar(15) NOT NULL,
+  `a_email` varchar(25) NOT NULL,
+  `a_pic` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`a_id`, `a_fname`, `a_mname`, `a_lname`, `a_suffix`, `a_add`, `a_contact`, `a_email`, `a_pic`) VALUES
+(1, 'Klevie', 'jun', 'caseres', 'JR', 'add', '7', 'ssssssss', ''),
+(2, 'Anthony', 'Loyd', 'Gonzales', '', 'Kalilangan', '98', 'mkjnhbsgsfeh', ''),
+(3, 'Princess', 'L', 'Tapayan', '', 'libona', '09888445562', 'ffdsfdsffewf', ''),
+(4, 'Anne', 'P', 'Cruz', '', 'asdsdsadad', '00222200102', 'fefwefwef', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(150) NOT NULL,
+  `abstract` varchar(1000) DEFAULT NULL,
+  `pub_date` date NOT NULL,
+  `department` smallint(11) NOT NULL,
+  `rev_count` smallint(6) DEFAULT NULL,
+  `status` varchar(15) NOT NULL,
+  `enabled` tinyint(4) NOT NULL,
+  `views_count` smallint(6) DEFAULT NULL,
+  `cover` varchar(80) NOT NULL,
+  `docloc` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`book_id`, `book_title`, `abstract`, `pub_date`, `department`, `rev_count`, `status`, `enabled`, `views_count`, `cover`, `docloc`) VALUES
+(1, 'This is Title', 'Abstract ni', '2018-08-01', 2, 0, 'Unpublish', 1, 0, 'cover/5b66f8dd0c8ce8.59408068.jpg', 'book/5b66f8dd0d2f38.89169842.d');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
+  `section` varchar(15) NOT NULL,
+  `description` varchar(40) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` smallint(6) NOT NULL,
+  `cat_name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `cat_name`) VALUES
+(1, 'I.T'),
+(2, 'Education');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dictionary`
+--
+
+CREATE TABLE `dictionary` (
+  `id` int(20) NOT NULL,
+  `word` varchar(45) NOT NULL,
+  `index_count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `indexes`
+--
+
+CREATE TABLE `indexes` (
+  `index_id` bigint(20) NOT NULL,
+  `word_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `offset` int(11) NOT NULL,
+  `end` tinyint(4) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `junc_advicerbook`
+--
+
+CREATE TABLE `junc_advicerbook` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `adv_id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `junc_advicerbook`
+--
+
+INSERT INTO `junc_advicerbook` (`id`, `book_id`, `adv_id`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `junc_authorbook`
+--
+
+CREATE TABLE `junc_authorbook` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `aut_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `junc_authorbook`
+--
+
+INSERT INTO `junc_authorbook` (`id`, `book_id`, `aut_id`) VALUES
+(1, 1, 1),
+(6, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `junc_bookkeywords`
+--
+
+CREATE TABLE `junc_bookkeywords` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `keywords_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `junc_bookkeywords`
+--
+
+INSERT INTO `junc_bookkeywords` (`id`, `book_id`, `keywords_id`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `junk_bookref`
+--
+
+CREATE TABLE `junk_bookref` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `webref_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `junk_bookref`
+--
+
+INSERT INTO `junk_bookref` (`id`, `book_id`, `webref_id`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keywords`
+--
+
+CREATE TABLE `keywords` (
+  `id` int(11) NOT NULL,
+  `key_words` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `keywords`
+--
+
+INSERT INTO `keywords` (`id`, `key_words`) VALUES
+(1, 'daeed'),
+(2, 'mfnsbvcvbn');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `name`, `nickname`, `email`, `password`) VALUES
+(1, 'anne', 'anne', 'anne@gmail.com', 'anne');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ref`
+--
+
+CREATE TABLE `ref` (
+  `id` int(11) NOT NULL,
+  `link` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ref`
+--
+
+INSERT INTO `ref` (`id`, `link`) VALUES
+(1, 'dambhdgye'),
+(2, 'efmnsbvcske');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username and password unique` (`u_name`,`password`);
+
+--
+-- Indexes for table `adviser`
+--
+ALTER TABLE `adviser`
+  ADD PRIMARY KEY (`adv_id`),
+  ADD UNIQUE KEY `adv_name` (`adv_fname`);
+
+--
+-- Indexes for table `authentication`
+--
+ALTER TABLE `authentication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`a_id`),
+  ADD UNIQUE KEY `author name unique` (`a_fname`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`),
+  ADD UNIQUE KEY `book name unique` (`book_title`),
+  ADD KEY `Book Has a Category` (`department`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dictionary`
+--
+ALTER TABLE `dictionary`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `word must unique` (`word`);
+
+--
+-- Indexes for table `indexes`
+--
+ALTER TABLE `indexes`
+  ADD PRIMARY KEY (`index_id`),
+  ADD UNIQUE KEY `word id book id offset unique` (`word_id`,`book_id`,`offset`),
+  ADD KEY `word_id` (`word_id`),
+  ADD KEY `index_id` (`index_id`),
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `offset` (`offset`);
+
+--
+-- Indexes for table `junc_advicerbook`
+--
+ALTER TABLE `junc_advicerbook`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `adv_id` (`adv_id`);
+
+--
+-- Indexes for table `junc_authorbook`
+--
+ALTER TABLE `junc_authorbook`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `AuthorBook` (`book_id`,`aut_id`),
+  ADD KEY `Pk_author_id` (`aut_id`);
+
+--
+-- Indexes for table `junc_bookkeywords`
+--
+ALTER TABLE `junc_bookkeywords`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book have manay key words` (`book_id`),
+  ADD KEY `many to many` (`keywords_id`);
+
+--
+-- Indexes for table `junk_bookref`
+--
+ALTER TABLE `junk_bookref`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Book` (`book_id`),
+  ADD KEY `WebRefence` (`webref_id`);
+
+--
+-- Indexes for table `keywords`
+--
+ALTER TABLE `keywords`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `keywordsUnique` (`key_words`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ref`
+--
+ALTER TABLE `ref`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT for table `adviser`
+--
+ALTER TABLE `adviser`
+  MODIFY `adv_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `authentication`
+--
+ALTER TABLE `authentication`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `dictionary`
+--
+ALTER TABLE `dictionary`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `indexes`
+--
+ALTER TABLE `indexes`
+  MODIFY `index_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `junc_advicerbook`
+--
+ALTER TABLE `junc_advicerbook`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `junc_authorbook`
+--
+ALTER TABLE `junc_authorbook`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `junc_bookkeywords`
+--
+ALTER TABLE `junc_bookkeywords`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `junk_bookref`
+--
+ALTER TABLE `junk_bookref`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `keywords`
+--
+ALTER TABLE `keywords`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ref`
+--
+ALTER TABLE `ref`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `Book Has a Category` FOREIGN KEY (`department`) REFERENCES `department` (`id`);
+
+--
+-- Constraints for table `indexes`
+--
+ALTER TABLE `indexes`
+  ADD CONSTRAINT `fk book id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk word id` FOREIGN KEY (`word_id`) REFERENCES `dictionary` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `junc_advicerbook`
+--
+ALTER TABLE `junc_advicerbook`
+  ADD CONSTRAINT `Pk_Advicer` FOREIGN KEY (`adv_id`) REFERENCES `adviser` (`adv_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Pk_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `junc_authorbook`
+--
+ALTER TABLE `junc_authorbook`
+  ADD CONSTRAINT `Pk_author_id` FOREIGN KEY (`aut_id`) REFERENCES `author` (`a_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `Pk_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `junc_bookkeywords`
+--
+ALTER TABLE `junc_bookkeywords`
+  ADD CONSTRAINT `book have manay key words` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `many to many` FOREIGN KEY (`keywords_id`) REFERENCES `keywords` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `junk_bookref`
+--
+ALTER TABLE `junk_bookref`
+  ADD CONSTRAINT `Book` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `WebRefence` FOREIGN KEY (`webref_id`) REFERENCES `ref` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
