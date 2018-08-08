8c62a78ee313bbe8cf2d14fad50998dd4b0b76b5
loydanthony <freddiemunio@gmail.com>
2018-08-09 01:53:27 +0800

    ACCESS CODES FUNCTIONING

--- accesscode.php
+++ accesscode.php
@@ -10,7 +10,7 @@
   $acctype = $_SESSION['type'];
   //echo $acctype;
 
-  
+
 
 
 
@@ -24,12 +24,14 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> Administrator </title>
+    <title> RESEARCH RECORD MANAGEMENT SYSTEM</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
     <!-- Custom Theme Style -->
     <link rel="stylesheet" type="text/css" media="screen" href="css/custom.min.css">
-	
+		<link rel="stylesheet" type="text/css" media="screen" href="css/button.css">
+
+
 </head>
 <body class="nav-md">
     <div class="container body">
@@ -60,8 +62,8 @@
                 <div class="nav side-menu">
 					<ul><a href="admindashboard.php"> DOCUMENTS </span></a></ul>
 					<ul><a href="updateAcc.php"> UPDATE ACCOUNT </a></ul>
-					<ul><a class= "dashboard-active" href="#code"> ACCESS CODE </a> </ul>      
-					<ul><a href="reports.php"> REPORTS </a> </ul> </br>      
+					<ul><a class= "dashboard-active" href="#code"> ACCESS CODE </a> </ul>
+					<ul><a href="reports.php"> REPORTS </a> </ul> </br>
 					<ul><button id= "btn-logout"><strong> <a href="#Logout"> LOGOUT </a></strong></button></ul>
                 </div>
               </div>
@@ -69,7 +71,7 @@
             </div>
           </div>
         </div>
-       
+
         <!-- page content -->
         <div class="right_col" role="main">
 			<div id= "admin-frm-container" class="frm-container" style="margin: auto; width: 80%">
@@ -77,7 +79,7 @@
 			<hr></br>
 			<form id= "admin-frm-generatepass" class= "frm-generatepass" action="/action_page.php">
 				<table style="font-size: 15px">
-					<tr> 
+					<tr>
 						<td> <b> Number of Access Code: </b> </td>
 						<td> <input type="number" placeholder="0" name="number" min="0" value="0" id="access-count" style= "width: 50%" required> </td>
 						<td> <button type="button" id="admin-btn-generate" class="btn-generate"> GENERATE </button> </td>
@@ -88,19 +90,33 @@
 			<hr>
 			</br> <center><h1> GENERATED CODES </h1></center>
 			<form id= "admin-frm-generatepass" class= "frm-generatepass" action="/action_page.php">
-				<table id="tbl-accescodes" style="font-size: 15px">
-					
+				<div id="printtable">
+				<table style="width:100%"border="1" cellpadding="3" id="tbl-accescodes" style="font-size: 15px" >
+
 					<tr class="access-tr-head">
 						<th id="access-th">Count</th>
 						<th id="access-th">Access Codes</th>
 						<th id="access-th">Type</th>
 					</tr>
 				</table>
+			</div>
+				<iframe name="print_frame" width="0" height="0" frameborder="0" src="about:blank"></iframe>
 			</form></br></br>
 
 			<hr></br>
 			<span style="float: left;">
-			<button type="submit" id= "admin-btn-print" class="btn-print"> PRINT </button>
+
+  	<!--	<button onclick="printContent('tbl-accescodes')">Print</button> -->
+		<button class="print-button" onclick="printDiv()"><span class="print-icon"></button>
+
+		<script>
+		function printDiv() {
+			 window.frames["print_frame"].document.body.innerHTML = document.getElementById("printtable").innerHTML;
+			 window.frames["print_frame"].window.focus();
+			 window.frames["print_frame"].window.print();
+		 }
+		</script>
+
 			</span>
 		</div>
           <!-- top tiles -->
@@ -122,6 +138,6 @@
 
     <!-- Custom Theme Scripts -->
     <script src="js/accesscode.js"></script>
-	
+
   </body>
 </html>
