c15243e31075cad791e769201de2c89f0c36136f
loydanthony <freddiemunio@gmail.com>
2018-08-09 00:03:23 +0800

    aug 9 12 am

---
+++ dw.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>&lt;html&gt;<br />
+  &lt;head&gt;<br />
+  &lt;title&gt;My Page&lt;/title&gt;<br />
+  &lt;/head&gt;<br />
+  &lt;body&gt;<br />
+  &lt;form name=&quot;myform&quot; action=&quot;#&quot; method=&quot;POST&quot;&gt;<br />
+  &lt;div align=&quot;center&quot;&gt;<br />
+  &lt;select name=&quot;mydropdown&quot;&gt;<br />
+  &lt;option value=&quot;Milk&quot;&gt;Fresh Milk&lt;/option&gt;<br />
+  &lt;option value=&quot;Cheese&quot;&gt;Old Cheese&lt;/option&gt;<br />
+  &lt;option value=&quot;Bread&quot;&gt;Hot Bread&lt;/option&gt;</p>
+<p>&lt;/select&gt;<br />
+  &lt;select name=&quot;mydropdown&quot;&gt;<br />
+  &lt;option value=&quot;Milk&quot;&gt;Fresh Milk&lt;/option&gt;<br />
+  &lt;option value=&quot;Cheese&quot;&gt;Old Cheese&lt;/option&gt;<br />
+  &lt;option value=&quot;Bread&quot;&gt;Hot Bread&lt;/option&gt;</p>
+<p>&lt;/select&gt;<br />
+  &lt;/div&gt;<br />
+  &lt;/form&gt;<br />
+  &lt;/body&gt;<br />
+  &lt;/html&gt;</p>
+</body>
+</html>
