884ca924931b6380552df1bff38a8291f310e3fa
himeTapayan <himesama.labuntog22@gmail.com>
2018-08-10 16:53:33 +0800

    back to home

--- updateAcc.php
+++ updateAcc.php
@@ -60,7 +60,8 @@
 					<ul><a href="admindashboard.php"> DOCUMENTS </span></a></ul>
 					<ul><a class= "dashboard-active" href="#update"> UPDATE ACCOUNT </a></ul>
 					<ul><a href="accesscode.php"> ACCESS CODE </a> </ul>
-					<ul><a href="fiddle.php"> REPORTS </a> </ul> </br>      
+					<ul><a href="fiddle.php" target="_blank"> REPORTS </a> </ul> </br>
+					<ul><a href="index(loyd).php"> Back to Home </a> </ul>      
 					<ul><button id= "btn-logout"><strong> <a href="#Logout"> LOGOUT </a></strong></button></ul>
                 </div>
               </div>
