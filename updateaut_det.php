058e2883d875929ebfdc7592f2d3abf64e00d470
Rayanne Cruz <rayannepelincruz@gmail.com>
2018-08-11 00:31:50 +0800

    DONE-UPDATE AUTHOR-UI

---
+++ updateaut_det.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- ANNE -->
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <title>Update Author - BUKSU RRMS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/updateaut_det.css">
+</head>
+
+
+<body class="updateautbody">
+
+    <div class="authorsearch">
+      <form class="authorform">
+        <input class="annesearchbox" type="text" placeholder="Search.." name="search">
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+
+
+          <div class="annefield">
+              <fieldset style="width:87%;">
+                  <legend style="font-size:14pt;"> AUTHOR DETAILS </legend>
+                      <input type="text" placeholder="FIRST NAME" name="fname"> <br> <br>
+                      <input type="text" placeholder="MIDDLE NAME" name="mname"> <br> <br>
+                      <input type="text" placeholder="LAST NAME" name="lname"> <br> <br>
+                      <input type="text" placeholder="ADDRESS" name="address"> <br> <br>
+                      <input type="text" placeholder="CONTACT" name="contact"> <br> <br>
+                      <input type="text" placeholder="EMAIL" name="email"> <br> <br>
+                      <button type="save">SAVE</button>
+              </fieldset>
+
+
+
+          </div>
+
+
+
+
+
+
+</body>
+</html>
