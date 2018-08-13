79a198e6dc6d3d1458f43114653b9195a8a553c6
Rayanne Cruz <rayannepelincruz@gmail.com>
2018-08-13 09:19:20 +0800

    DONE UI

---
+++ supporting.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- ANNE -->
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <title>Supporting Documents - BUKSU RRMS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/supporting.css">
+</head>
+<header>
+   <?php
+        include_once 'header.php';
+     ?>
+
+</header>
+<body id="supportbody">
+<div class="annesupport">
+    <h1 class="annename"> RAYANNE CRUZ </h1>
+  </div>
+  <div class="annedocument" >
+              <h3> SUPPORTING DOCUMENTS </h3>
+                  <hr>
+  </div>
+
+            <div class="supportdocu">
+                      <ul class="anneul">
+
+                      <li> <a  href="#">Disseminated Certificate </a> </li> <br>
+
+                      </ul>
+                  </div>
+                  <br/>
+                  <br/>
+
+                  <hr>
+
+
+
+</body>
+<footer style="padding-top: 5px;">
+  <?php include_once 'footer.php' ?>
+
+</footer>
+</html>
