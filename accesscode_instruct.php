3abc066991a4f9593d565181325133fd1a8135d8
himeTapayan <himesama.labuntog22@gmail.com>
2018-08-20 04:14:14 +0800

    Instructor Dashboard

--- accesscode_instruct.php
+++ accesscode_instruct.php
@@ -55,11 +55,15 @@ session_start();
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
                 <div class="nav side-menu">
-					<ul><a href="instructordashboard.php"> DOCUMENTS </span></a></ul>
-					<ul><a class= "dashboard-active" href="#code"> ACCESS CODE </a> </ul>      
-					<ul><a href="fiddle.php" target="_blank"> REPORTS </a> </ul> </br>
-					<ul><a href="index.php"> Back to Home </a> </ul>      
-					<ul><button id= "btn-logout"><strong> <a href="#Logout"> LOGOUT </a></strong></button></ul>
+					<ul><a class= "dashboard-active" href="#documents"> MY RESEARCH </span></a></ul>
+					<ul><a href="accesscode_instruct.php"> ACCESS CODE </a> </ul>
+					 <?php
+                            $d = Date('Y-m-d');
+                            $yr = explode("-", $d);
+                            echo '<ul><a href="book_reports.php?title=&dept=&status=&author=&from=0&to=' . $yr[0] . '" target="_blank"> REPORTS </a> </ul>';
+                          ?>
+					</br>
+					<ul><a href="index.php"> Back to Home </a> </ul>
                 </div>
               </div>
 
@@ -75,8 +79,9 @@ session_start();
 				<table style="width= 100%">
 					<tr> 
 						<td width= "50%"> <b> Number of Access Code: </b> </td>
-						<td> <input type="number" placeholder="0" id="access-count" name="number" min="0" style= "width: 50%; font-size: 14pt" required> </td>
-						<td><button type="submit" id= "instructor-frm-generate" class="btn-generate" style="font-size:12pt"> Generate </button></td>
+						<td> <input type="number" placeholder="0" id="access-count" name="number" min="0" style= "width: 50%; font-size: 13pt; padding: 2%;" required> </td>
+						<td><button type="submit" id= "instructor-frm-generate" class="btn-generate" 
+						style="font-size: 12pt; font-weight: bold; padding: 10%; border-radius: 10%;"> Generate </button></td>
 					</tr>
 				</table>
 			<hr></br>
@@ -116,7 +121,8 @@ session_start();
 				<br>
 				<br>
 			<hr>
-			<button type="submit" id= "instructor-btn-print" class="btn-print" style="font-size: 14pt" onclick="printDiv()"> PRINT </button>
+			<button type="submit" id= "instructor-btn-print" class="btn-print" 
+			style= "font-size: 12pt; font-weight: bold; padding: 2% 5% 2% 5%; border-radius: 10%;" onclick="printDiv()"> PRINT </button>
 			
 		</div>
           <!-- top tiles -->
