b19009d1b91568d9e47976316a2665a088fb83db
Rayanne Cruz <rayannepelincruz@gmail.com>
2018-08-20 03:25:16 +0800

    UI modified-again

--- dept.php
+++ dept.php
@@ -76,7 +76,6 @@
 					<ul><a href="accesscode.php"> ACCESS CODE </a> </ul>
 					<?php
                             $d = Date('Y-m-d');
-<<<<<<< HEAD
 
                             $yr = explode("-", $d);
 
@@ -85,12 +84,10 @@
                             $yr = explode("-", $d);
 
 
-
-=======
                             $yr = explode("-", $d);
 
 
->>>>>>> 5c83b21fe1bdef6bdde78f976a9c6e17c81101eb
+
                             echo '<ul><a href="book_reports.php?title=&dept=&status=&author=&from=0&to=' . $yr[0] . '" target="_blank"> REPORTS </a> </ul>';
                           ?>
 
@@ -104,7 +101,7 @@
         </div>
 
         <!-- page content -->
-        <div class="right_col" role="main" style= "min-height: 712px;>
+        <div class="right_col" role="main" style= "min-height: 950px;>
 
 
           <div class="login-page">
